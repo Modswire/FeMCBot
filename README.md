076019b5c0e7f8a8385a8506b6dd2620802443af
LiloviaLilossy <ddlc.femc@gmail.com>
2020-03-22 18:02:29 +0300

    v4.0 init

--- README.md
+++
@@ -1,3 +0,0 @@
-# FeMCBot
-Just a small DDLC-related Discord.py bot.
-Her standard prefixes is: `f_`, `femc ` and `FeMC `.
--- corev2.py
+++ corev2.py
@@ -1,29 +1,12 @@
+from addons.get import get_token, get_prefix, get_owner
+from discord.ext import commands
 
-# Libs
-import discord
-import asyncio
-import json
-import os
-from addons.functions import *
-from discord.ext import commands, tasks
-from random import randint, choice
+bot = commands.Bot(
+	command_prefix=get_prefix,
+	description="Just a DDLC-related bot.",
+	owner_ids=get_owner())
+token = get_token()
 
-
-# bot defs
-token = get_token(tokentype="token")
-bot = commands.Bot(command_prefix=get_prefix)
-bot.owners = get_owners
-bot.log = logsending
-bot.coglist = get_cogs
-
-bot.remove_command("help")
-bot.load_extension('jishaku')
-for cog in bot.coglist():
-    bot.load_extension(cog)
-
-
-@bot.event
-async def on_ready():
-    print(f'I am ready!')
+bot.load_extension("jishaku")
 
 bot.run(token)
