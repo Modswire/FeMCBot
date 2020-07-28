e1b0a81e738cc31b1ad000b8a201c88904989956
LiloviaLilossy <ddlc.femc@gmail.com>
2020-07-28 07:23:01 +0300

    fixing indentation

--- corev2.py
+++ corev2.py
@@ -10,24 +10,24 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 class FeMCBot(commands.Bot):
-	def __init__(self):
-		logger.info("Initializing the bot")
-		super().__init__(command_prefix=get_prefix, owner_id=321566831670198272)
-		logger.info("Loading jishaku")
-		self.load_extension("jishaku")
-		for cog in get_cogs():
-			try:
-				self.load_extension(cog)
-				logger.info(f"Loaded extension {cog}")
-			except:
-				logger.warning(f"I was unable to load extension {cog}", exc_info=1)
-	
-	async def on_ready(self):
-		logger.info("Logged in as "+bot.user.name)
-	
-	async def on_error(self, event, *args, **kwargs):
-		logger.error("There's an error!", exc_info=1)
-		
+    def __init__(self):
+        logger.info("Initializing the bot")
+        super().__init__(command_prefix=get_prefix, owner_id=321566831670198272)
+        logger.info("Loading jishaku")
+        self.load_extension("jishaku")
+        for cog in get_cogs():
+            try:
+                self.load_extension(cog)
+                logger.info(f"Loaded extension {cog}")
+            except:
+                logger.warning(f"I was unable to load extension {cog}", exc_info=1)
+    
+    async def on_ready(self):
+        logger.info("Logged in as "+bot.user.name)
+    
+    async def on_error(self, event, *args, **kwargs):
+        logger.error("There's an error!", exc_info=1)
+        
 
 bot = FeMCBot()
 bot.run(get_token())
