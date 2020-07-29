0998b8b45875732002b53b06b9cd84c25de5b430
LiloviaLilossy <ddlc.femc@gmail.com>
2020-07-29 09:27:15 +0300

    sending all the logs to single file

--- cogs/ddmc/ddmc.py
+++ cogs/ddmc/ddmc.py
@@ -6,10 +6,8 @@ from random import randint
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='logs/ddmc.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 
 class WebsiteCog(commands.Cog):
     def __init__(self, bot):
