f12200342d4257d5ec9888e989980bd224563957
LiloviaLilossy <ddlc.femc@gmail.com>
2020-12-22 10:32:37 +0300

    more visual improvements

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from datetime import datetime
 from jishaku.modules import package_version
+import humanize
 import sys
 
 from typing import TYPE_CHECKING
@@ -12,26 +13,31 @@ if TYPE_CHECKING:
 class MiscCog(commands.Cog):
     def __init__(self, bot: "FeMCBot"):
         self.bot = bot
+        self.rfemc = None
 
     @commands.command()
     async def about(self, ctx):
         e = await self.bot.embed
         femc = self.bot
-        rfemc = await femc.reddit.user.me()
+        if not self.rfemc:
+            self.rfemc = await femc.reddit.user.me()
+
         bi = f"""
         **Bot Owners:** {", ".join([i.mention for i in femc.owners])}
 
-        **Uptime:** {datetime.now() - femc.starttime}
+        **Uptime:** {humanize.naturaltime(datetime.now() - femc.starttime)}
         **Python version:** {sys.version}
         **discord.py version:** enhanced-dpy {package_version("enhanced-dpy")}
         **aPRAW version:** {package_version("apraw")}
         """
+
         di = f"""
         **Discord Username:** {femc.user.name}#{femc.user.discriminator}
         **Discord ID:** {femc.user.id}
         """
+
         ri = f"""
-        **Reddit Username:** u/{rfemc.name}
+        **Reddit Username:** u/{self.rfemc.name}
         """
         e.add_field(name="__Bot Info__", value=bi)
         e.add_field(name="__Discord Info__", value=di, inline=False)
